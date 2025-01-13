0e614811acbd050f1a72f89dc4c7b22b98bd83e6
Aleks Gomeniuk <agsfinks@gmail.com>
2025-01-13 10:55:46 +0000

    feat: isContainNumber

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -252,8 +252,12 @@ function getIndexOf(str, letter) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  const number = `${num}`;
+  for (let i = 0; i < number.length; i += 1) {
+    if (number[i] === `${digit}`) return true;
+  }
+  return false;
 }
 
 /**
