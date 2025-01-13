c2df8b22ef2d68e9cb56ee0135a34ca76d2eef43
Aleks Gomeniuk <agsfinks@gmail.com>
2025-01-13 12:49:31 +0000

    feat: shuffleChar

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -443,8 +443,22 @@ function sortByAsc(/* arr */) {
  *  '012345', 3 => '024135' => '043215' => '031425'
  *  'qwerty', 3 => 'qetwry' => 'qtrewy' => 'qrwtey'
  */
-function shuffleChar(/* str, iterations */) {
-  throw new Error('Not implemented');
+function shuffleChar(str, iterations) {
+  let result = str;
+
+  for (let i = 0; i < iterations; i += 1) {
+    let evenStr = '';
+    let oddStr = '';
+
+    for (let j = 0; j < result.length; j += 1) {
+      if (j % 2 === 0) evenStr += result[j];
+      else oddStr += result[j];
+    }
+
+    result = evenStr + oddStr;
+  }
+
+  return result;
 }
 
 /**
